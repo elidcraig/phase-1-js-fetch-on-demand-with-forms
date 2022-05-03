de242df8b95b5244972b06465fa0e4a91915d0f2
Eli Craig <elidcraig@gmail.com>
2022-05-02 20:15:09 -0700

    added event listner to submit, added fetch request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', event => {
+        event.preventDefault()
+        const input = event.target['searchByID']
+        // console.log(input.value)
+        
+        fetch('http://localhost:3000/movies')
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+// Add event listeners to capture form data
+// Fetch data based on what the user types into that form
+// Display that data on the page
